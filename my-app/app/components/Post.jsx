3ddf4ab68c20771c1516d1a9745ace00228f7734
yang <ysotgood@gmail.com>
2023-03-08 01:45:10 +0900

    스토리색션-포스트 이미지 추가하기

--- my-app/app/components/Post.jsx
+++ my-app/app/components/Post.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { DocumentPlusIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 
 export default function Post({ img, userImg, caption, username, id }) {
+    console.log(username);
     return (
-        <div>
-            <h1>{username}</h1>
+        <div className="bg-white border rounded-md my-7">
+            <div className="flex items-center p-5">
+                <Image className="object-cover p-1 mr-3 border rounded-full" src={userImg} alt={username} width={100} height={100}></Image>
+                <p className="flex-1 font-bold">{username}</p>
+                <DocumentPlusIcon className="h-5" />
+            </div>
+            <Image className="w-full " src={img} alt="" width={280} height={280}></Image>
+            {/* Image 태그에 object-fit:cover,fill 속성 제거해봄 
+            추후에 주의해서 볼 것 */}
         </div>
     );
 }
--- my-app/next.config.js
+++ my-app/next.config.js
@@ -4,7 +4,15 @@ const nextConfig = {
         appDir: true,
     },
     images: {
-        domains: ['emilyannebondoc.files.wordpress.com', 'upload.wikimedia.org', 'www.jennexplores.com'],
+        domains: [
+            'emilyannebondoc.files.wordpress.com',
+            'upload.wikimedia.org',
+            'www.jennexplores.com',
+            'pbs.twimg.com',
+            'mblogthumb-phinf.pstatic.net',
+            'mobiinsidecontent.s3.ap-northeast-2.amazonaws.com',
+            'encrypted-tbn0.gstatic.com',
+        ],
     },
 };
 
