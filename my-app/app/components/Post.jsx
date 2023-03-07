0d65986f44ae7ce6729871aedade2ebc13bec371
yang <ysotgood@gmail.com>
2023-03-08 02:15:57 +0900

    스토리색션-아이콘 추가

--- my-app/app/components/Post.jsx
+++ my-app/app/components/Post.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DocumentPlusIcon } from '@heroicons/react/24/outline';
+import { DocumentPlusIcon, HeartIcon, ChatBubbleLeftIcon, BookmarkIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 
 export default function Post({ img, userImg, caption, username, id }) {
@@ -7,13 +7,26 @@ export default function Post({ img, userImg, caption, username, id }) {
     return (
         <div className="bg-white border rounded-md my-7">
             <div className="flex items-center p-5">
-                <Image className="object-cover p-1 mr-3 border rounded-full" src={userImg} alt={username} width={100} height={100}></Image>
+                <Image
+                    className="object-cover p-1 mr-3 border rounded-full cursor-pointer"
+                    src={userImg}
+                    alt={username}
+                    width={100}
+                    height={100}
+                ></Image>
                 <p className="flex-1 font-bold">{username}</p>
                 <DocumentPlusIcon className="h-5" />
             </div>
             <Image className="w-full " src={img} alt="" width={280} height={280}></Image>
             {/* Image 태그에 object-fit:cover,fill 속성 제거해봄 
             추후에 주의해서 볼 것 */}
+            <div className="flex justify-between px-4 pt-4">
+                <div className="flex space-x-4 ">
+                    <HeartIcon className="btn" />
+                    <ChatBubbleLeftIcon className="btn" />
+                </div>
+                <BookmarkIcon className="btn" />
+            </div>
         </div>
     );
 }
