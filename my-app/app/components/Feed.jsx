9d09b420fe0d980ceeb71e867913dd7211b2d4ed
yang <ysotgood@gmail.com>
2023-03-08 04:02:46 +0900

    스토리색션-색선1,색션2 분리(grid md:사용)

--- my-app/app/components/Feed.jsx
+++ my-app/app/components/Feed.jsx
@@ -3,12 +3,12 @@ import Stories from './Stories';
 
 export default function Feed() {
     return (
-        <main>
-            <section>
+        <main className="grid grid-cols-1 mx-auto md:grid-cols-3 md:max-w-6xl">
+            <section className="md:col-span-2">
                 <Stories></Stories>
                 <Posts></Posts>
             </section>
-            <section></section>
+            <section className="hidden md:inline-grid md:col-span-1"></section>
         </main>
     );
 }
