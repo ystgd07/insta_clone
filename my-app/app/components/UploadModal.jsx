205cf0dad8174d10cd471ecd8e58d51bca08936b
yang <ysotgood@gmail.com>
2023-03-23 17:33:24 +0900

    feature modal- firebase에 이미지와 캡션 저장하기(firestore+storage)

--- my-app/app/components/UploadModal.jsx
+++ my-app/app/components/UploadModal.jsx
@@ -4,10 +4,39 @@ import { useRecoilState } from 'recoil';
 import Modal from 'react-modal';
 import { CameraIcon } from '@heroicons/react/24/outline';
 import { useRef, useState } from 'react';
+import { async } from '@firebase/util';
+import { addDoc, collection, doc, serverTimestamp, updateDoc } from 'firebase/firestore';
+import { db, storage } from '@/firebase';
+import { useSession } from 'next-auth/react';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 export default function UploadModal() {
     const [open, setOpen] = useRecoilState(modalState);
     const [selectedFile, setSelectedFile] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const { data: session } = useSession();
+    async function uploadPost() {
+        if (loading) return;
+        setLoading(true);
+        const docRef = await addDoc(collection(db, 'posts'), {
+            caption: captionRef.current.value,
+            username: session.user.username,
+            profileImg: session.user.image,
+            timestamp: serverTimestamp(),
+        });
+        const imageRef = ref(storage, `posts/${docRef.id}/image`);
+        await uploadString(imageRef, selectedFile, 'data_url').then(async (snapshot) => {
+            const downloadURL = await getDownloadURL(imageRef);
+            await updateDoc(doc(db, 'posts', docRef.id), {
+                image: downloadURL,
+            });
+        });
+        setOpen(false);
+        setLoading(false);
+        setSelectedFile(null);
+    }
     const filePickerRef = useRef(null);
+    const captionRef = useRef(captionRef);
+
     function addImageToPost(event) {
         const reader = new FileReader();
         if (event.target.files[0]) {
@@ -17,6 +46,7 @@ export default function UploadModal() {
             setSelectedFile(readerEvent.target.result);
         };
     }
+
     return (
         <div>
             {open && (
@@ -49,9 +79,11 @@ export default function UploadModal() {
                             maxLength="150"
                             placeholder="Please enter your caption..."
                             className="w-full m-4 text-center border-none focus:ring-0 "
+                            ref={captionRef}
                         />
                         <button
-                            disabled
+                            disabled={!selectedFile || loading}
+                            onClick={uploadPost}
                             className="w-full p-2 text-white bg-red-600 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100 "
                         >
                             Upload Post
--- my-app/firebase.js
+++ my-app/firebase.js
@@ -9,13 +9,13 @@ import { getAnalytics } from 'firebase/analytics';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STR_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MSG_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MST_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STR_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MSG_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MST_ID,
 };
 
 // Initialize Firebase
@@ -23,5 +23,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
 const storage = getStorage();
 
-const analytics = getAnalytics(app);
 export { app, db, storage };
